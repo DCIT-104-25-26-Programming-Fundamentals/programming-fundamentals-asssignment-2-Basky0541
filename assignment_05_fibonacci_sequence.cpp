756147df1d502bfc8d7e501f8b80ff3ea6698f95
Basky0541 <sbasky@st.ug.edu.gh>
2026-07-29 19:15:40 +0000

    Add Fibonacci functions

--- assignment_05_fibonacci_sequence.cpp
+++ assignment_05_fibonacci_sequence.cpp
@@ -51,3 +51,71 @@
 #include <iostream>
 using namespace std;
 
+#include <iostream>
+using namespace std;
+
+// Part A: Print the first N Fibonacci terms
+void printFibonacci(int n)
+{
+    if (n <= 0)
+    {
+        cout << "Error: N must be a positive integer." << endl;
+        return;
+    }
+
+    int first = 0, second = 1, next;
+
+    cout << "Fibonacci sequence: ";
+
+    for (int i = 1; i <= n; i++)
+    {
+        cout << first << " ";
+
+        next = first + second;
+        first = second;
+        second = next;
+    }
+
+    cout << endl;
+}
+
+// Part B: Check if a number is a Fibonacci number
+void checkFibonacci(int num)
+{
+    if (num < 0)
+    {
+        cout << num << " is NOT a Fibonacci number." << endl;
+        return;
+    }
+
+    int first = 0, second = 1, next;
+
+    while (first < num)
+    {
+        next = first + second;
+        first = second;
+        second = next;
+    }
+
+    if (first == num)
+        cout << num << " is a Fibonacci number." << endl;
+    else
+        cout << num << " is NOT a Fibonacci number." << endl;
+}
+
+int main()
+{
+    int n, number;
+
+    // Part A
+    cout << "How many terms? ";
+    cin >> n;
+    printFibonacci(n);
+
+    // Part B
+    cout << "Enter a number to check: ";
+    cin >> number;
+    checkFibonacci(number);
+
+    return 0;
+}
